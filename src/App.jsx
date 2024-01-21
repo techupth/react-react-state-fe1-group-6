f11df8c63a56b991324ed607ce303c6325e7f9c6
GaiKT <124340777+GaiKT@users.noreply.github.com>
2024-01-21 09:46:07 +0700

    feat: create usestate and event button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [message , setMessage] = useState('Greeting Message')
+
+  const userHandle = (word) => {
+    setMessage(word)
+  }
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{message}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={(event)=>{
+          userHandle(event.target.value)
+        }} value={'สวัสดี!'}>สวัสดี!</button>
+        <button onClick={(event)=>{
+          userHandle(event.target.value)
+        }} value={'Hi!'}>Hi!</button>
+        <button onClick={(event)=>{
+          userHandle(event.target.value)
+        }} value={'你好!'}>你好!</button>
       </div>
     </div>
   );
